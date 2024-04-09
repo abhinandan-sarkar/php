b357c473fa1a8ed4281b2bda34f5de93a5015ad5
Abhinandan Sarkar <84449356+abhinandan-sarkar@users.noreply.github.com>
2024-04-09 20:47:34 +0530

    Create class.php
    1

---
+++ class.php
@@ -0,0 +1,16 @@
+#class is the blue print of a oject
+
+class Mobile{
+public $model_number;
+private $original_price;
+public $market_price;
+public $ram;
+public $rom;
+public $front_camara;
+public $rear_camara;
+public $brand;
+
+public function turn_on_phone(){
+  echo "hello $brand";
+}
+}
