06df61d50404ba3b0625f7198b7614fabdba3809
Abhinandan <abhinandansarkar00@gmail.com>
2024-05-18 22:03:41 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$title="index page";
+include("htmlTemp.php");
+?>
+<fieldset class="w-100">
+    <table border="1">
+        <tr >
+            <td class="w-50"><img src="images/logo.png" alt="" width="50px" height="50px"></td>
+            <td class=" w-i text-center">Home</td>
+            <td class=" text-center">About</td>
+            <td class=" text-center">Service</td>
+            <td class=" text-center">Contact</td>
+            <td class=" text-center">Service</td>
+        </tr>
+    </table>
+</fieldset>
+<fieldset class="w-75">
+
+</fieldset>
+<fieldset class="w-100">
+
+</fieldset>
+<fieldset class="w-100">
+
+</fieldset>
---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+$title="signup page";
+include("htmlTemp.php");
+?>
+<div class="center">
+<fieldset>
+    <legend>Sign Up</legend>
+    <form action="">
+        <table>
+          
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" id=""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="pasd" id=""></td>
+            </tr>
+
+            <tr >
+                <td  class="  "><input type="submit" value="Submit"></td>
+                <td><input type="reset" value="Reset"></td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+<fieldset>
+    <legend>Sign Up</legend>
+    <form action="">
+        <table>
+          
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email" id=""></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="pasd" id=""></td>
+            </tr>
+
+            <tr >
+                <td  class="  "><input type="submit" value="Submit"></td>
+                <td><input type="reset" value="Reset"></td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+</div>
